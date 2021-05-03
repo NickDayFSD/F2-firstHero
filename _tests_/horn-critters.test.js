46b869b60558ae23f0654c62c0555c70cddc3ea6
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-05-03 16:28:20 -0700

    all tests passing

--- _tests_/horn-critters.test.js
+++ _tests_/horn-critters.test.js
@@ -173,3 +173,18 @@ test('GET /api/horns', async () => {
   expect(response.status).toBe(200);
   expect(response.body).toEqual(expected);
 });
+
+test('GET /api/horns/1', async () => {
+  const expected = {
+    url:
+      'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
+    title: 'UniWhal',
+    description: 'A unicorn and a narwhal nuzzling their horns',
+    keyword: 'narwhal',
+    horns: 1
+  };
+  const response = await request.get('/api/horns/1');
+
+  expect(response.status).toBe(200);
+  expect(response.body).toEqual(expected);
+});
